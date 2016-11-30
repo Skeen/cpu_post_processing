cf5f391967005c25681613a3447012dfb722de68
Emil 'Skeen' Madsen <sovende@gmail.com>
2016-11-30 16:47:16 +0100

    Accept input via UNIX pipes.

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,53 @@
+#!/usr/bin/env node
+'use strict';
+
 import {Processor} from './processor';
 import {Reading} from './channel';
 
-var fs = require('fs');
-var data = JSON.parse(fs.readFileSync(process.argv[2], 'utf8'));
+var readline = require('readline');
 
-var proces = new Processor();
-var readings : Reading[] = proces.process_data(data);
-
-console.log("Time, Value");
-readings.forEach(function(reading)
+var read_timeseries = function(callback)
 {
-    console.log(reading.time + ", " + reading.result);
+    var rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout,
+      terminal: false
+    });
+
+    var input = "";
+    rl.on('line', function(line)
+    {
+        input += line;
+    });
+
+    rl.on('close', function()
+    {
+        var json;
+        try
+        {
+            json = JSON.parse(input);
+        }
+        catch(err)
+        {
+            console.error();
+            console.error("Fatal error: Piped input is not valid JSON!");
+            console.error();
+            console.error(err);
+            process.exit(1);
+        }
+
+        callback(json);
+    });
+};
+
+read_timeseries(function(json)
+{
+    var proces = new Processor();
+    var readings : Reading[] = proces.process_data(json);
+
+    console.log("Time, Value");
+    readings.forEach(function(reading)
+    {
+        console.log(reading.time + ", " + reading.result);
+    });
 });
